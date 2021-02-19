b4acf1be067aa80726416aad27e744964ba6910a
Paul Veliscovici <paul.veliscovici@gmail.com>
2021-02-19 00:44:18 -0500

    backend design done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :find_task, only: [:show, :edit, :update, :destroy]
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
   def index
     @restaurants = Restaurant.all
   end
@@ -16,19 +16,23 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
 
-    redirect_to task_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def edit; end
 
   private
 
-  def find_task
-    @task = Task.find(params[:id])
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def restaurant_params
-    params.require(:task).permit(:title, :details, :completed)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :new, :create, :show, :edit]
+  resources :restaurants, only: [:index, :new, :create, :show, :edit] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
